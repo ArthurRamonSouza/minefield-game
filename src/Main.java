5bcbd0216b408698c2c24cbce5802e60dc53d12b
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2022-12-01 19:32:59 -0300

    Some tests code to QA.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,28 @@ public class Main {
         Gameboard gb = new Gameboard(Difficulty.EASY);
         gb.generateBomb();
         //gb.printing();
+        //Testing generateBombs();
+
+        Bomb[] bombs = gb.getBombs();
+        Space[][] spaces = gb.getSpaces();
+
+        
+        for(int i = 0; i < 10; i++){
+            System.out.printf("%d - bomb(%d,%d).\n", i, bombs[i].getCoordinateX(), bombs[i].getCoordinateY());
+        }
+
+        /*Counting the number of bombs
+        int count = 0;
+        for(int i = 0; i < (gb.getLength()-1); i++){
+            for(int j = 0; j < (gb.getLength()-1); j++){
+               if(spaces[i][j].hasBomb()){
+                    count++;
+               }
+            }
+        }
+        System.out.printf("%d bombs in the game.\n", count);
+        */
+       
         gb.printBoardConsole();
     
     }
