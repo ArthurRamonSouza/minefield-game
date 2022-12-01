747f924423f2b86506ded751bd9153f1e1f8a3d8
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2022-12-01 15:04:25 -0300

    Testing new method.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int max = 10;
-
-        for(int i = 0; i < (max-1); i++){
-            System.out.println("max: " + max);
-        
-            System.out.println(i);
-
-        }
 
         Gameboard gb = new Gameboard(Difficulty.EASY);
         gb.generateBomb();
-        gb.printing();
+        //gb.printing();
+        gb.printBoardConsole();
+    
     }
 }
