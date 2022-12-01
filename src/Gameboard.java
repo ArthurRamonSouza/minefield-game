3e5887badd3471d5c1d86600c3716d67aa2c884d
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2022-12-01 19:32:31 -0300

    Paramter 'bound' of random was altered and fixes.

--- src/Gameboard.java
+++ src/Gameboard.java
@@ -5,18 +5,18 @@ public class Gameboard {
     //This array multidimensional represents the spaces on the gameboard.
     //Space is a separeted class with attributes and methods.
     private Space[][] spaces;
-
+    
     //Total of spaces of the board
     private int maxSize;
-
+    
     //Quantity of spaces N in our board NxN
     private int length;
 
     //This array will contain the coordinates of the bombs
     private Bomb[] bombs = new Bomb[10];
-
+    
     Gameboard(Difficulty dificulty){
-
+        
         if(Difficulty.EASY == dificulty){
             spaces = new Space[8][8];
             maxSize = 64;
@@ -34,7 +34,7 @@ public class Gameboard {
             maxSize = 196;
             length = 14;
         }
-
+        
         //Initializing the spaces in the board
         for(int i = 0; i < length; i++){
             for(int j = 0 ; j < length; j++){
@@ -43,24 +43,43 @@ public class Gameboard {
         }
     }
 
+    public Bomb[] getBombs() {
+        return bombs;
+    }
+    
+    public Space[][] getSpaces() {
+        return spaces;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
     public void generateBomb(){
         Random random = new Random();
         
-        for(int i = 0; i < 10; i++){
-            bombs[i] = new Bomb((random.nextInt(0, maxSize-1)),(random.nextInt(0, maxSize-1)));
+        for(int i = 0; i < bombs.length; i++){
+            bombs[i] = new Bomb((random.nextInt(0, length)),(random.nextInt(0, length)));
         }
 
         //Checking if we have two or more bombs in the same position (x,y), if yes, we change the coordinates 
-        for(int i = 1; i < 10; i++){
+        for(int i = 1; i < bombs.length; i++){
             for(int j = 0; j < i; j++){
                 if((bombs[i].getCoordinateX() == bombs[j].getCoordinateX()) && (bombs[i].getCoordinateY() == bombs[j].getCoordinateY())){
-                    bombs[j].setCoordinateX(random.nextInt(0, maxSize-1));
-                    bombs[j].setCoordinateY(random.nextInt(0, maxSize-1));
+                    bombs[j].setCoordinateX(random.nextInt(0, length));
+                    bombs[j].setCoordinateY(random.nextInt(0, length));
                     i=-1;
                 }
             }
         }
 
+        //Planting the bombs
+        for(int i = 0; i < bombs.length; i++){
+            int x = bombs[i].getCoordinateX();
+            int y = bombs[i].getCoordinateY();
+            spaces[x][y].addBomb();
+        }
+        
     }
 
     /*
@@ -87,16 +106,16 @@ public class Gameboard {
         char unicode = '\u25A0';
 
         for(int line = 0; line < length; line++){
-            for(int i = 0; i < length; i++){
-                if(!spaces[line][i].isHidden()){
+            for(int column = 0; column < length; column++){
+                if(!spaces[line][column].isHidden()){
                     unicode = '\u25A1';
+                }
+                if(spaces[line][column].hasBomb()){
+                    unicode = 'X';
                 }
                 System.out.print(unicode + " ");
             }
             System.out.println();
         }
-
-
     }
-
 }
