c13db21a053687e69a352787f8c530f872eff17c
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2022-12-03 13:07:16 -0300

    New attribute to count the number of bombs near.

--- src/Space.java
+++ src/Space.java
@@ -1,6 +1,15 @@
 public class Space {
     private boolean bomb = false;
     private boolean hidden = true;
+    private int bombsNear = 0;
+
+    public int getBombsNear() {
+        return bombsNear;
+    }
+
+    public void addBombsNear() {
+        this.bombsNear++;
+    }
 
     public void addBomb(){
         bomb = true;
